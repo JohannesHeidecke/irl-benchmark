88b4e270e4fd2f5c2883d8ee2bae8a7581d6dd4e
Johannes Heidecke <jheidecke@gmail.com>
2018-10-11 23:02:45 +0200

    enable PPO for discrete state space

--- irl_benchmark/rl/algorithms/ppo.py
+++ irl_benchmark/rl/algorithms/ppo.py
@@ -1,8 +1,10 @@
 '''Proximal Policy Optimization'''
 from gym.spaces import Box
+from gym.spaces import Discrete as DiscreteSpace
 import numpy as np
 
 from irl_benchmark.rl.algorithms import SLMAlgorithm
+from irl_benchmark.utils.utils import to_one_hot
 
 
 class PPO(SLMAlgorithm):
@@ -16,6 +18,9 @@ class PPO(SLMAlgorithm):
         )
 
     def pick_action(self, state):
+        if isinstance(self.env.observation_space, DiscreteSpace):
+            # state needs to be one-hot encoded to be suitable neural network input
+            state = to_one_hot(state, self.env.observation_space.n)
         action = self.agent.algorithm.act(state)
         if isinstance(self.env.action_space, Box) and np.isscalar(action):
             # Box action spaces require even single actions to be a list:
