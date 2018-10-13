7267384974d4d31c97c03314766a1cb61b6daf3d
Max Daniel <max.marian.daniel@gmail.com>
2018-10-13 03:22:03 +0200

    hopefully improved MaxEnt IRL (still not performing well though)

--- examples/meirl_experiments.py
+++ examples/meirl_experiments.py
@@ -7,25 +7,24 @@ from irl_benchmark.irl.feature.feature_wrapper import FrozenLakeFeatureWrapper
 from irl_benchmark.irl.reward.reward_function import FeatureBasedRewardFunction
 from irl_benchmark.irl.reward.reward_wrapper import RewardWrapper
 from irl_benchmark.rl.algorithms.value_iteration import ValueIteration
-from irl_benchmark.utils.utils import get_transition_matrix
+from irl_benchmark.utils.utils import get_transition_matrix, avg_undiscounted_return
 
 store_to = 'data/frozen/expert/'
 no_episodes = 1000
-max_steps_per_episode = 1000
+max_steps_per_episode = 500
 
-env = gym.make('FrozenLake8x8-v0')
+env = gym.make('FrozenLake-v0')
 env = FrozenLakeFeatureWrapper(env)
-initial_reward_function_estimate = FeatureBasedRewardFunction(env=env, parameters=np.zeros(64))
-env = RewardWrapper(env=env, reward_function=initial_reward_function_estimate)
-
 # Generate expert trajectories.
 expert_agent = ValueIteration(env)
 print('Training expert agent...')
 expert_agent.train(30)
 expert_trajs = collect_trajs(env, expert_agent, no_episodes,
                              max_steps_per_episode, store_to)
+initial_reward_function_estimate = FeatureBasedRewardFunction(env=env, parameters=np.zeros(16))
+env = RewardWrapper(env=env, reward_function=initial_reward_function_estimate)
 
-feat_map = np.eye(64)
+feat_map = np.eye(16)
 
 transition_dynamics = get_transition_matrix(env)
 
@@ -34,10 +33,26 @@ def rl_alg_factory(env):
     return ValueIteration(env)
 
 
-meirl = MaxEnt(env,  expert_trajs=expert_trajs, transition_dynamics=transition_dynamics, rl_alg_factory=rl_alg_factory)
-rewards = meirl.train(feat_map, time_limit=60, verbose=True)
+expert_performance = avg_undiscounted_return(expert_trajs)
+print('The expert ' +
+      'reached the goal in ' + str(expert_performance) + ' of trajs.')
 
+meirl = MaxEnt(env, expert_trajs=expert_trajs, transition_dynamics=transition_dynamics, rl_alg_factory=rl_alg_factory, learning_rate=1e-1)
+print('Running MaxEnt IRL...')
+rewards = meirl.train(feat_map, time_limit=120, verbose=False)
 rewards = (rewards - np.min(rewards))/(np.max(rewards) - np.min(rewards))
 
+print('Normalized reward coefficients returned by MaxEnt IRL:')
 print(rewards)
 
+# Check how often the goal state was reached.
+test_agent = rl_alg_factory(meirl.env)
+print('Training test agent w/ reward obtained by MaxEnt...')
+test_agent.train(30)
+print('Done training test agent.')
+test_trajs = collect_trajs(meirl.env, test_agent, no_episodes,
+                           max_steps_per_episode,
+                           store_to='data/frozen/foo')
+irl_performance = avg_undiscounted_return(test_trajs)
+print('The test agent trained w/ the reward estimated by MaxEnt IRL ' +
+      'reached the goal in ' + str(irl_performance) + ' of trajs.')
--- irl_benchmark/irl/algorithms/maxent/me_irl.py
+++ irl_benchmark/irl/algorithms/maxent/me_irl.py
@@ -4,7 +4,7 @@ import time
 
 from irl_benchmark.irl.algorithms.base_algorithm import BaseIRLAlgorithm
 from irl_benchmark.irl.reward.reward_function import FeatureBasedRewardFunction
-from irl_benchmark.utils.utils import unwrap_env
+from irl_benchmark.utils.utils import unwrap_env, is_unwrappable_to
 
 
 class MaxEnt(BaseIRLAlgorithm):
@@ -19,38 +19,65 @@ class MaxEnt(BaseIRLAlgorithm):
                  expert_trajs,
                  transition_dynamics,
                  rl_alg_factory,
-                 gamma=0.99,
-                 lr=0.02):
-        '''Initialize Maximum Entropy IRL algorithm. '''
-        super(MaxEnt, self).__init__(env, expert_trajs, rl_alg_factory)
-        # make sure env is DiscreteEnv (other cases not implemented yet
-        # TODO: implement other cases
-        unwrap_env(env, gym.envs.toy_text.discrete.DiscreteEnv)
+                 learning_rate=0.02):
+        '''Initialize Maximum Entropy IRL algorithm.
 
-        self.gamma = gamma
+        Args:
+        env -- `RewardWrapper` around a DiscreteEnv from OpenAI Gym
+        expert_trajs -- `list` of expert trajectories as dictionaries
+        transition_dynamics -- `np.ndarray` of shape [n_states, n_actions,
+                               n_states],
+                               matrix of transition probabilities in env
+        rl_alg_factory -- `function` taking an env and returning an RL agent
+        learning_rate -- `float`, learning rate for gradient descent
+        '''
+        super(MaxEnt, self).__init__(env, expert_trajs, rl_alg_factory)
+        # make sure env is DiscreteEnv (other cases not implemented yet)
+        # TODO: implement other cases
+        is_unwrappable_to(env, gym.envs.toy_text.discrete.DiscreteEnv)
+
         self.transition_dynamics = transition_dynamics
-        self.n_states, self.n_actions, _ = np.shape(self.transition_dynamics)
-        self.lr = lr
+        self.n_states = unwrap_env(env, gym.Env).observation_space.n
+        self.n_actions = unwrap_env(env, gym.Env).action_space.n
+        self.learning_rate = learning_rate
 
     def expected_svf(self, policy):
         '''Calculate the expected state visitation frequency for the trajectories
         under the given policy.
 
-        Returns vector of state visitation frequencies.
-        '''
+        Args:
+        policy -- `np.ndarray` of shape [self.n_states, self.n_actions],
+                  entry [s, a] is prob of taking action a in state s
 
+        Returns:
+        mu -- `np.ndarray` of shape (self.n_states, ),
+              vector of approximated state visitation frequencies
+        '''
         # get the length of longest trajectory:
         longest_traj_len = 1  # init
         for traj in self.expert_trajs:
             longest_traj_len = max(longest_traj_len, len(traj['states']))
+        longest_traj_len *= 100  # To catch policies that take longer.
 
         # mu[s, t] is the prob of visiting state s at time t
         mu = np.zeros((self.n_states, longest_traj_len))
 
+        # Get empirical initial state distribution.
         for traj in self.expert_trajs:
             mu[traj['states'][0], 0] += 1
         mu[:, 0] = mu[:, 0] / len(self.expert_trajs)
 
+        # Calculate the state-visit freqs based on policy & dynamics.
+        #
+        # Note that this isn't exact because we ignore trajectories that
+        # have positive probability but are longer than any expert trajectory.
+        #
+        # The approximation should be good when the input policy is
+        # close to the expert one and we use a large number of expert
+        # trajectories, but can be bad otherwise.
+        #
+        # We ad hoc mitigated this issue above by considering trajectories
+        # up to 10 times the length of the longest expert trajectory.
         for t in range(1, longest_traj_len):
             for s in range(self.n_states):
                 tot = 0
@@ -59,39 +86,42 @@ class MaxEnt(BaseIRLAlgorithm):
                         tot += mu[pre_s, t - 1] * self.transition_dynamics[
                             pre_s, action, s] * policy[pre_s, action]
                 mu[s, t] = tot
-        return np.sum(mu, 1)
+        mu = np.sum(mu, axis=1)
+        return mu
 
     def train(self,
               feat_map,
               time_limit=300,
               rl_time_per_iteration=15,
               verbose=False):
-        """
-        Maximum Entropy Inverse Reinforcement Learning (Maxent IRL)
-        inputs:
-          feat_map    NxD matrix - the features for each state
-          P_a         NxN_ACTIONSxN matrix - P_a[s0, a, s1] is the transition prob of
-                                             landing at state s1 when taking action
-                                             a at state s0
-          gamma       float - RL discount factor
-          trajs       a list of demonstrations
-          lr          float - learning rate
-          n_iters     int - number of optimization steps
-        returns
-          rewards     Nx1 vector - recovered state rewards
+        """Maximum Entropy Inverse Reinforcement Learning (MaxEnt IRL).
+
+        Args:
+        feat_map -- `np.ndarray` of shape [self.n_states, k],
+                    row s contains k-dimensional features for state s
+        time_limit -- `int`, total IRL training time in seconds
+        rl_time_per_iteration -- `int`, per-iteration training time of the
+                                 Reinforcement Learning step
+        verbose -- `bool`, print stuff if true
+
+        Returns:
+        rewards -- `np.ndarray` of shape (self.n_states, ),
+                   recovered state rewards
         """
         t0 = time.time()
 
         # init parameters
         theta = np.random.uniform(size=(feat_map.shape[1], ))
 
-        # calc feature expectations
+        # calc undiscounted feature expectations
         feat_exp = np.zeros([feat_map.shape[1]])
         for episode in self.expert_trajs:
             for state in episode['states']:
                 feat_exp += feat_map[state]
         feat_exp = feat_exp / len(self.expert_trajs)
-        #print(feat_exp)
+        if verbose:
+            print('Undiscounted expert feature expectations:')
+            print(feat_exp)
 
         agent = self.rl_alg_factory(self.env)
 
@@ -99,7 +129,7 @@ class MaxEnt(BaseIRLAlgorithm):
         iteration_counter = 0
         while time.time() < t0 + time_limit:
             iteration_counter += 1
-            if verbose:
+            if verbose and iteration_counter % 20 == 0:
                 print('iteration: {}'.format(iteration_counter))
 
             reward_function_estimate = FeatureBasedRewardFunction(
@@ -109,17 +139,16 @@ class MaxEnt(BaseIRLAlgorithm):
             # compute policy
             agent.train(time_limit=rl_time_per_iteration)
 
+            # Note that this currently only works for value_iteration.
             policy = agent.pi
 
             # compute state visitation frequencies
             svf = self.expected_svf(policy)
 
             # compute gradients
-            grad = -(feat_exp - feat_map.T.dot(svf))
+            grad = feat_exp - feat_map.T.dot(svf)
 
             # update params
-            theta += self.lr * grad
+            theta += self.learning_rate * grad
 
-        # return sigmoid(normalize(rewards))
-        self.reward_function = reward_function_estimate
         return theta
