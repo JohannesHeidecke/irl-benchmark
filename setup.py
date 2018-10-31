d9acf60ed23b4a2aa2e838d16dc140fcb3c92241
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-11-01 00:38:24 +0530

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
-from setuptools import find_packages
 
-setup(name='irl-benchmark',
-      version='0.0.1',
-      description='IRL Benchmarking',
-      author='Johannes Heidecke',
-      install_requires=['gym', 'numpy', 'cvxpy'],
-      packages=find_packages())
+setup(
+    name='irl-benchmark',
+    version='0.1',
+    packages=['', 'old', 'old.rl', 'old.rl.algorithms', 'old.irl', 'old.irl.reward', 'old.irl.collect',
+              'old.irl.feature', 'old.irl.algorithms', 'old.envs', 'old.utils', 'old.metrics', 'examples',
+              'irl_benchmark', 'irl_benchmark.rl', 'irl_benchmark.rl.algorithms', 'irl_benchmark.irl',
+              'irl_benchmark.irl.reward', 'irl_benchmark.irl.collect', 'irl_benchmark.irl.feature',
+              'irl_benchmark.irl.algorithms', 'irl_benchmark.utils', 'irl_benchmark.config', 'irl_benchmark.metrics',
+              'irl_benchmark.experiment'],
+    url='',
+    license='',
+    author='Anton Osika, Adria Garriga-Alonso, Johannes Heidecke, Max Daniel, Sayan Sarkar',
+    author_email='',
+    description=''
+)
