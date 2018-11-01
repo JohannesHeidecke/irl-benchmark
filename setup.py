1779bddcbd6558c2760988610912898fffb25666
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-11-02 00:05:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ from setuptools import setup
 
 setup(
     name='irl-benchmark',
-    version='0.1',
-    packages=['', 'old', 'old.rl', 'old.rl.algorithms', 'old.irl', 'old.irl.reward', 'old.irl.collect',
-              'old.irl.feature', 'old.irl.algorithms', 'old.envs', 'old.utils', 'old.metrics', 'examples',
-              'irl_benchmark', 'irl_benchmark.rl', 'irl_benchmark.rl.algorithms', 'irl_benchmark.irl',
+    version='',
+    packages=['', 'examples', 'irl_benchmark', 'irl_benchmark.rl', 'irl_benchmark.rl.algorithms', 'irl_benchmark.irl',
               'irl_benchmark.irl.reward', 'irl_benchmark.irl.collect', 'irl_benchmark.irl.feature',
               'irl_benchmark.irl.algorithms', 'irl_benchmark.utils', 'irl_benchmark.config', 'irl_benchmark.metrics',
               'irl_benchmark.experiment'],
